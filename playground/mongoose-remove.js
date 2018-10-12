4a3d7877e9a370cd901d702face29d5875e9782a
Rabel Obispo <rabelobispo@hotmail.com>
2018-10-11 20:03:48 -0400

    Testing findOneAndDelete

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -6,9 +6,13 @@ const { Todo } = require("../server/models/models");
 console.clear();
 
 // Todo.deleteMany({})
-//   .then(todos => console.log("find:\n", todos))
+//   .then(todos => console.log("DeteleMany:\n", todos))
 //   .catch(e => console.log(e));
 
-Todo.findByIdAndDelete("5bbfdb66fbc74adad32ad064")
-  .then(todo => console.log("Remove:\n", todo))
+// Todo.findByIdAndDelete("5bbfdb66fbc74adad32ad064")
+//   .then(todo => console.log("FindByIdAndDelete:\n", todo))
+//   .catch(e => console.log(e));
+
+Todo.findOneAndDelete("5bbfe47efbc74adad32ad1f7")
+  .then(todo => console.log("FindOneAndDelete:\n", todo))
   .catch(e => console.log(e));
